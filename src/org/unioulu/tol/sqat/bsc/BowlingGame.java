f33017c1635b22eed7dcdfcb051a0dfa825f22c0
somename <someemail>
2015-10-15 12:19:17 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -8,7 +8,10 @@ public class BowlingGame {
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
 	
-	public BowlingGame(){}
+	public BowlingGame()
+	{
+		
+	}
 	
 	public void addFrame(Frame frame){
 		frames.add(frame);
