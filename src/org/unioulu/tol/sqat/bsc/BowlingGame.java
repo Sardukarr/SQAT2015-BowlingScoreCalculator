087dbed2edc0cbc480f97aa35445702bf9937d2d
wstudenc <wstudenc@STUPID-PC515.stnet.local>
2015-10-15 13:40:29 +0300

    finish

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -3,6 +3,8 @@ package org.unioulu.tol.sqat.bsc;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// finish 1;36
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
